33a39051079a53b5ad936b804c9f2242dae8c2d4
Arumugam J <aj27134@gmail.com>
2016-08-23 16:23:51 -0400

    added token input

--- phabric-ui/src/net/blackphoton/phabric/ui/wizard/PhabricRepositorySettingsPage.java
+++ phabric-ui/src/net/blackphoton/phabric/ui/wizard/PhabricRepositorySettingsPage.java
@@ -1,5 +1,8 @@
 package net.blackphoton.phabric.ui.wizard;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.preference.StringFieldEditor;
 import org.eclipse.mylyn.tasks.core.RepositoryTemplate;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
 import org.eclipse.mylyn.tasks.ui.wizards.AbstractRepositorySettingsPage;
@@ -10,28 +13,49 @@ import net.blackphoton.phabric.core.PhabricatorCorePlugin;
 
 public class PhabricRepositorySettingsPage extends AbstractRepositorySettingsPage implements ITaskRepositoryPage {
 
+	private StringFieldEditor lblToken;
+
 	public PhabricRepositorySettingsPage(TaskRepository repository) {
 		super("Phabricator Repository Settings", "Settings for Phabricator repository", repository);
 		setNeedsAnonymousLogin(false);
 		setNeedsEncoding(false);
 		setNeedsTimeZone(false);
-		setNeedsProxy(false);
-		setNeedsAdvanced(false);
+		setNeedsRepositoryCredentials(false);
 	}
 
 	@Override
 	public void createControl(Composite parent) {
 		super.createControl(parent);
 		addRepositoryTemplatesToServerUrlCombo();
+
+		lblToken = new StringFieldEditor("apiToken", "API Token", StringFieldEditor.UNLIMITED,
+				innerComposite.getContent()) {
+			@Override
+			protected void valueChanged() {
+				super.valueChanged();
+				if (getWizard() != null) {
+					getWizard().getContainer().updateButtons();
+				}
+			}
+
+			@Override
+			public int getNumberOfControls() {
+				return 2;
+			}
+		};
+	}
+
+	@Override
+	public boolean isPageComplete() {
+		// make sure "Automatic" is not selected as a version
+		return super.isPageComplete() && lblToken != null && lblToken.getStringValue() != null
+				&& lblToken.getStringValue() != "";
 	}
 
 	@Override
 	protected void repositoryTemplateSelected(RepositoryTemplate template) {
 		repositoryLabelEditor.setStringValue(template.label);
 		setUrl(template.repositoryUrl);
-		setUserId("user");
-		setPassword("pass");
-		
 		getContainer().updateButtons();
 	}
 
@@ -43,6 +67,16 @@ public class PhabricRepositorySettingsPage extends AbstractRepositorySettingsPag
 	@Override
 	protected void createAdditionalControls(Composite parent) {
 		// TODO Auto-generated method stub
+
+	}
+
+	public class PhabricatorValidator extends Validator {
+
+		@Override
+		public void run(IProgressMonitor monitor) throws CoreException {
+			// TODO Auto-generated method stub
+
+		}
 
 	}
 
