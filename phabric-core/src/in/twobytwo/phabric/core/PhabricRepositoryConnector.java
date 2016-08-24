f9e021a64acc1ae80219d9f700e9f6827c45af09
jsixface <aj27134@gmail.com>
2016-08-24 14:26:59 -0400

    changing the package name and vendor from blackphoton.net to twobytwo.in

---
+++ phabric-core/src/in/twobytwo/phabric/core/PhabricRepositoryConnector.java
@@ -0,0 +1,89 @@
+package in.twobytwo.phabric.core;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.mylyn.tasks.core.AbstractRepositoryConnector;
+import org.eclipse.mylyn.tasks.core.IRepositoryQuery;
+import org.eclipse.mylyn.tasks.core.ITask;
+import org.eclipse.mylyn.tasks.core.TaskRepository;
+import org.eclipse.mylyn.tasks.core.data.TaskData;
+import org.eclipse.mylyn.tasks.core.data.TaskDataCollector;
+import org.eclipse.mylyn.tasks.core.sync.ISynchronizationSession;
+
+public class PhabricRepositoryConnector extends AbstractRepositoryConnector {
+
+	@Override
+	public boolean canCreateNewTask(TaskRepository repository) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canCreateTaskFromKey(TaskRepository repository) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getConnectorKind() {
+		return PhabricatorCorePlugin.CONNECTOR_KIND;
+	}
+
+	@Override
+	public String getLabel() {
+		return "Phabricator Repository";
+	}
+
+	@Override
+	public String getRepositoryUrlFromTaskUrl(String taskUrl) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TaskData getTaskData(TaskRepository repository, String taskId, IProgressMonitor monitor)
+			throws CoreException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTaskIdFromTaskUrl(String taskUrl) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTaskUrl(String repositoryUrl, String taskIdOrKey) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasTaskChanged(TaskRepository taskRepository, ITask task, TaskData taskData) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public IStatus performQuery(TaskRepository repository, IRepositoryQuery query, TaskDataCollector collector,
+			ISynchronizationSession session, IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateRepositoryConfiguration(TaskRepository taskRepository, IProgressMonitor monitor)
+			throws CoreException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTaskFromTaskData(TaskRepository taskRepository, ITask task, TaskData taskData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ phabric-ui/src/in/twobytwo/phabric/ui/wizard/PhabricRepositorySettingsPage.java
@@ -0,0 +1,83 @@
+package in.twobytwo.phabric.ui.wizard;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.mylyn.tasks.core.RepositoryTemplate;
+import org.eclipse.mylyn.tasks.core.TaskRepository;
+import org.eclipse.mylyn.tasks.ui.wizards.AbstractRepositorySettingsPage;
+import org.eclipse.mylyn.tasks.ui.wizards.ITaskRepositoryPage;
+import org.eclipse.swt.widgets.Composite;
+
+import in.twobytwo.phabric.core.PhabricatorCorePlugin;
+
+public class PhabricRepositorySettingsPage extends AbstractRepositorySettingsPage implements ITaskRepositoryPage {
+
+	private StringFieldEditor lblToken;
+
+	public PhabricRepositorySettingsPage(TaskRepository repository) {
+		super("Phabricator Repository Settings", "Settings for Phabricator repository", repository);
+		setNeedsAnonymousLogin(false);
+		setNeedsEncoding(false);
+		setNeedsTimeZone(false);
+		setNeedsRepositoryCredentials(false);
+	}
+
+	@Override
+	public void createControl(Composite parent) {
+		super.createControl(parent);
+		addRepositoryTemplatesToServerUrlCombo();
+
+		lblToken = new StringFieldEditor("apiToken", "API Token", StringFieldEditor.UNLIMITED,
+				innerComposite.getContent()) {
+			@Override
+			protected void valueChanged() {
+				super.valueChanged();
+				if (getWizard() != null) {
+					getWizard().getContainer().updateButtons();
+				}
+			}
+
+			@Override
+			public int getNumberOfControls() {
+				return 2;
+			}
+		};
+	}
+
+	@Override
+	public boolean isPageComplete() {
+		// make sure "Automatic" is not selected as a version
+		return super.isPageComplete() && lblToken != null && lblToken.getStringValue() != null
+				&& lblToken.getStringValue() != "";
+	}
+
+	@Override
+	protected void repositoryTemplateSelected(RepositoryTemplate template) {
+		repositoryLabelEditor.setStringValue(template.label);
+		setUrl(template.repositoryUrl);
+		getContainer().updateButtons();
+	}
+
+	@Override
+	public String getConnectorKind() {
+		return PhabricatorCorePlugin.CONNECTOR_KIND;
+	}
+
+	@Override
+	protected void createAdditionalControls(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public class PhabricatorValidator extends Validator {
+
+		@Override
+		public void run(IProgressMonitor monitor) throws CoreException {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+}
