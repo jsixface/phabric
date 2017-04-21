9670abbe5360fdb731dbef41f55f8a8e6d2cc0b8
Arumugam J <aj27134@gmail.com>
2017-04-21 18:10:37 -0400

    Added query page
    This just adds a query page with no controls. Next stage is to add the
    controls needed for search

--- phabric-ui/src/in/twobytwo/phabric/ui/wizard/Messages.java
+++ phabric-ui/src/in/twobytwo/phabric/ui/wizard/Messages.java
@@ -16,4 +16,8 @@ public class Messages extends NLS {
 
 	public static String QueryPage_Enter_Query;
 	public static String QueryPage_Description;
+
+	public static String FilterQueryPage_New_Trac_Query;
+	public static String FilterQueryPage_Add_search_filters_to_define_query;
+	public static String FilterQueryPage_Query_Title;
 }
